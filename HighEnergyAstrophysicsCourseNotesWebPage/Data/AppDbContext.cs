519d22a3ed6f632043cb5a3c271865181754563e
Aleyna Nur Öztürk <nurozturkaleyna@gmail.com>
2025-09-04 17:07:08 +0300

    Initial commit

---
+++ HighEnergyAstrophysicsCourseNotesWebPage/Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using HighEnergyAstrophysicsCourseNotesWebPage.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HighEnergyAstrophysicsCourseNotesWebPage.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public AppDbContext() : base(GetOptions())
+        {
+        }
+
+        private static DbContextOptions GetOptions()
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return builder.Options;
+        }
+    }
+}
